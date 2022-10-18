275eba54dbccf9fd61355b4bc2dd112f3cbb4a54
oURMIo <dimach.98.ru@gmail.com>
2022-10-18 18:32:18 +0300

    try call

--- threadDaemon/src/main/java/Homework.java
+++ threadDaemon/src/main/java/Homework.java
@@ -10,12 +10,13 @@ public class Homework {
             t.setDaemon(true);
             return t;
         });
-
         Future<?> future = null;
+        Future[] future1 = new Future[THREAD_COUNT];
+
         for (int i = 0; i < THREAD_COUNT; i++) {
             int finalI = i;
-            future = executorService.submit(() -> {
-                executorService.submit(new RunnableUser(userCounter1, finalI));
+            future1[i] = future = executorService.submit(() -> {
+                executorService.submit((Runnable) new RunnableUser(userCounter1, finalI));
             });
         }
 
@@ -27,6 +28,11 @@ public class Homework {
             }
         }
         executorService.shutdown();
+
+        System.out.printf("%n%n%n");
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            System.out.printf(" %s", future1[i].get());
+        }
     }
 }
 
@@ -37,7 +43,7 @@ interface Counter {
     long getValue();
 }
 
-class RunnableUser implements Runnable {
+class RunnableUser implements Runnable, Callable {
     private final Counter counter;
     private final int id;
 
@@ -48,10 +54,15 @@ class RunnableUser implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 0; i < 20; i++) {
-            counter.increment();
-        }
+/*        for (int i = 0; i < 1; i++) {
+        }*/
+        counter.increment();
         System.out.printf(" CountUser %s = %s %n", id, counter.getValue());
+    }
+
+    @Override
+    public Object call() throws Exception {
+        return counter.getValue();
     }
 }
 
